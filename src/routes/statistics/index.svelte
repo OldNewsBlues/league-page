f86aeddcf33fa473ce3e687abcdc89929ac1ef0d
OldNewsBlues <53295790+OldNewsBlues@users.noreply.github.com>
2021-09-26 20:29:07 -0500

    Update index.svelte

--- src/routes/statistics/index.svelte
+++ src/routes/statistics/index.svelte
@@ -1,14 +1,14 @@
 <script context="module">
 	import { getLeagueStatistics, getLeagueTransactions, waitForAll } from '$lib/utils/helper';
     export async function load() {
-        const recordsInfo = waitForAll(
+        const statisticsInfo = waitForAll(
 			getLeagueStatistics(),
 			getLeagueTransactions(false),
 		)
 	
 		return {
 			props: {
-				recordsInfo
+				statisticsInfo
 			}
 		};
 	}
@@ -17,7 +17,7 @@
 <script>
 	import LinearProgress from '@smui/linear-progress';
 	import { Statistics } from '$lib/components';
-    export let recordsInfo;
+    export let statisticsInfo;
 </script>
 
 <style>
@@ -34,14 +34,14 @@
 </style>
 
 <div id="main">
-    {#await recordsInfo}
+    {#await statisticsInfo}
         <!-- promise is pending -->
         <div class="loading">
             <p>Loading league records...</p>
             <LinearProgress indeterminate />
         </div>
-    {:then [leagueRecords, {totals, stale}]}
-        <Statistics {leagueRecords} {totals} {stale} />
+    {:then [leagueStatistics, {totals, stale}]}
+        <Statistics {leagueStatistics} {totals} {stale} />
     {:catch error}
         <!-- promise was rejected -->
         <p>Something went wrong: {error.message}</p>
