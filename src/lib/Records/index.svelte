0d8ee41c38dc0b23c28242248af5e773d453242e
OldNewsBlues <53295790+OldNewsBlues@users.noreply.github.com>
2021-09-27 18:44:30 -0500

    Update index.svelte

--- src/lib/Records/index.svelte
+++ src/lib/Records/index.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { getLeagueRecords, getLeagueTransactions } from '$lib/utils/helper';
+    import { getLeagueRecords, getLeagueTransactions } from '$lib/utils/helper'; //j
 
     import AllTimeRecords from './AllTimeRecords.svelte';
     import PerSeasonRecords from './PerSeasonRecords.svelte';
