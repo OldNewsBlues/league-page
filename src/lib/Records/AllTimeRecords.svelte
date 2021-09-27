f8c23206b52473abe08cd6e30015f075a9e8db78
OldNewsBlues <53295790+OldNewsBlues@users.noreply.github.com>
2021-09-26 20:07:34 -0500

    Update AllTimeRecords.svelte

--- src/lib/Records/AllTimeRecords.svelte
+++ src/lib/Records/AllTimeRecords.svelte
@@ -1,15 +1,12 @@
 <script>
     import {round} from '$lib/utils/helper'
-  	import StatisticsAndRankings from './StatisticsAndRankings.svelte';
-
-    export let leagueRosterStatistics, leagueWeekStatistics, currentManagers, mostSeasonLongPoints, transactionTotals;
-
+  	import RecordsAndRankings from './RecordsAndRankings.svelte';
+    export let leagueRosterRecords, leagueWeekRecords, currentManagers, mostSeasonLongPoints, transactionTotals;
     let winPercentages = [];
     let lineupIQs = [];
     const fptsHistories = [];
     const tradesData = [];
     const waiversData = [];
-
     let showTies = false;
     
     for(const rosterID in transactionTotals.allTime) {
@@ -24,42 +21,36 @@
             waivers: transactionTotals.allTime[rosterID].waiver,
         })
     }
-
-
-    for(const key in leagueRosterStatistics) {
-        const leagueRosterStatistic = leagueRosterStatistics[key];
+    for(const key in leagueRosterRecords) {
+        const leagueRosterRecord = leagueRosterRecords[key];
         winPercentages.push({
             rosterID: key,
             manager: currentManagers[key],
-            percentage: round((leagueRosterStatistic.wins + leagueRosterStatistic.ties / 2) / (leagueRosterStatistic.wins + leagueRosterStatistic.ties + leagueRosterStatistic.losses) * 100),
-            wins: leagueRosterStatistic.wins,
-            ties: leagueRosterStatistic.ties,
-            losses: leagueRosterStatistic.losses,
+            percentage: round((leagueRosterRecord.wins + leagueRosterRecord.ties / 2) / (leagueRosterRecord.wins + leagueRosterRecord.ties + leagueRosterRecord.losses) * 100),
+            wins: leagueRosterRecord.wins,
+            ties: leagueRosterRecord.ties,
+            losses: leagueRosterRecord.losses,
         })
-
         let lineupIQ = {
             rosterID: key,
             manager: currentManagers[key],
-            fpts: round(leagueRosterStatistic.fptsFor),
+            fpts: round(leagueRosterRecord.fptsFor),
         }
-
-        if(leagueRosterStatistic.potentialPoints) {
-            lineupIQ.iq = round(leagueRosterStatistic.fptsFor / leagueRosterStatistic.potentialPoints * 100);
-            lineupIQ.potentialPoints = round(leagueRosterStatistic.potentialPoints);
+        if(leagueRosterRecord.potentialPoints) {
+            lineupIQ.iq = round(leagueRosterRecord.fptsFor / leagueRosterRecord.potentialPoints * 100);
+            lineupIQ.potentialPoints = round(leagueRosterRecord.potentialPoints);
         }
-
         lineupIQs.push(lineupIQ)
     
         fptsHistories.push({
             rosterID: key,
             manager: currentManagers[key],
-            fptsFor: round(leagueRosterStatistic.fptsFor),
-            fptsAgainst: round(leagueRosterStatistic.fptsAgainst),
+            fptsFor: round(leagueRosterRecord.fptsFor),
+            fptsAgainst: round(leagueRosterRecord.fptsAgainst),
         })
     
-        if(leagueRosterStatistic.ties > 0) showTies = true;
+        if(leagueRosterRecord.ties > 0) showTies = true;
     }
-
     winPercentages.sort((a, b) => b.percentage - a.percentage);
     lineupIQs.sort((a, b) => b.iq - a.iq);
     fptsHistories.sort((a, b) => b.fptsFor - a.fptsFor);
@@ -67,10 +58,10 @@
     waiversData.sort((a, b) => b.waivers - a.waivers);
 </script>
 
-<StatisticsAndRankings
+<RecordsAndRankings
     {currentManagers}
-    weekStatistics={leagueWeekStatistics}
-    seasonLongStatistics={mostSeasonLongPoints}
+    weekRecords={leagueWeekRecords}
+    seasonLongRecords={mostSeasonLongPoints}
     {showTies}
     {winPercentages}
     {fptsHistories}
