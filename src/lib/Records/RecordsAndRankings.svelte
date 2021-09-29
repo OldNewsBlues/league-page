3210686dbf9907939a84902df272a594a76c1061
OldNewsBlues <53295790+OldNewsBlues@users.noreply.github.com>
2021-09-29 04:26:22 -0500

    Update RecordsAndRankings.svelte

--- src/lib/Records/RecordsAndRankings.svelte
+++ src/lib/Records/RecordsAndRankings.svelte
@@ -118,46 +118,9 @@
         })
     }
 
-    //let recordOffset = 0;
-//     const victories = [
-//         "Narrowest Wins",
-//     ]
-//     if(blowouts && blowouts.length) {
-//         recordOffset = 1;
-//     } else {
-//         victories.unshift('Largest Blowouts');
-//     }
-//     const changeVictorie = () => {
-//     	switch () {
-// 	    case 0 - recordOffset:
-// 		curDiff = 0;
-// 		break;
-// 	    case 1 - recordOffset:
-// 		curDiff = 1 - recordOffset;
-// 		break;
-// 	    default:
-// 		curDiff = 0;
-// 		break;
-// 	}
-//     }
-	
-    let displayDiff = [];
-    let curDiff = 'largblow';
-    
-    const changeVictorie = () => {
-        if(curDiff == 'largblow') {
-	    displayDiff = largestBlow;
-	} else {
-	    displayDiff = narrowWin;
-	}
-    }		
-    
-   $: changeVictorie(curDiff);
-		
-	
     let curTable = 0;
     let curGraph = 0;
-	
+
     let iqOffset = 0;
     const tables = [
         "Win Percentages",
@@ -220,11 +183,35 @@
                 break;
         }
     }
+    
+//     const changeRecordDisplay = (r) => { //Jesse Not top 10
+//     	if(r == 'recordhighs') {
+//         	displayRecord = 0;
+//         } else if(recorddisplay == 'recordhighs') {
+//         	displayRecord = 1;
+//         }
+//         recorddisplay = r;
+//     }
+    
+//     let recorddisplay = 'recordhighs';
+    
+//     const changeRecordYearDisplay = (r) => { //Jesse Not top 10
+//         if(r == 'recordyearhighs') {
+//         	displayRecordYear = 0;
+//         } else if(recordyeardisplay == 'recordhighs') {
+//             displayRecordYear = 1;
+//         }
+//         recordyeardisplay = r;
+//     }
+    
+//     let recordyeardisplay = 'recordyearhighs';
 
     $: changeTable(curGraph);
     $: changeGraph(curTable);
     
     let innerWidth;
+//     let displayRecord = 0; //Jesse not top 10
+//     let displayRecordYear = 0;
 
 </script>
 
@@ -500,120 +487,48 @@
             {/each}
         </Body>
     </DataTable>
-</div>
 
-<div class="rankingHolder">
-    <div class="rankingInner" style="margin-left: -{100 * curDiff}%;">
-	    {#if displayDiff == 'largestBlow'}
-		{#if blowouts && blowouts.length}
-			<div class="rankingTableWrapper">
-				<DataTable class="rankingTable">
-				    <Head>
-					<Row>
-					    <Cell class="header" colspan=4>{prefix} Largest Blowouts</Cell>
-					</Row>
-					<Row>
-					    <Cell class="header rank"></Cell>
-					    <Cell class="header">Matchup</Cell>
-					    <Cell class="header">Week</Cell>
-					    <Cell class="header">Diff</Cell>
-					</Row>
-				    </Head>
-				    <Body>
-					{#each blowouts as blowout, ix}
-					    <Row>
-						<Cell class="rank">{ix + 1}</Cell>
-						<Cell class="cellName center differentialName">
-						    <div class="center" on:click={() => gotoManager(blowout.home.rosterID)}>
-							{blowout.home.manager.name} ({round(blowout.home.fpts)})
-							{#if !allTime  && cleanName(blowout.home.manager.name) != cleanName(currentManagers[blowout.home.rosterID].name)}
-							    <div class="curRecordManager">({currentManagers[blowout.home.rosterID].name})</div>
-							{/if}
-						    </div>
-						    vs
-						    <div class="center" on:click={() => gotoManager(blowout.away.rosterID)}>
-							{blowout.away.manager.name} ({round(blowout.away.fpts)})
-							{#if !allTime  && cleanName(blowout.away.manager.name) != cleanName(currentManagers[blowout.away.rosterID].name)}
-							    <div class="curRecordManager">({currentManagers[blowout.away.rosterID].name})</div>
-							{/if}
-						    </div>
-						</Cell>
-						<Cell>{allTime ? blowout.year + " - " : "" } {blowout.week}</Cell>
-						<Cell>{round(blowout.differential)}</Cell>
-					    </Row>
-					{/each}
-				    </Body>
-				</DataTable>
-			</div>
-		{/if}
-	    {:else}
-		{#if closestMatchups && closestMatchups.length}
-		    <div class="rankingTableWrapper">
-			<DataTable class="rankingTable">
-			    <Head>
-				<Row>
-				    <Cell class="header" colspan=4>{prefix} Narrowest Wins</Cell>
-				</Row>
-				<Row>
-				    <Cell class="header rank"></Cell>
-				    <Cell class="header">Matchup</Cell>
-				    <Cell class="header">Week</Cell>
-				    <Cell class="header">Diff</Cell>
-				</Row>
-			    </Head>
-			    <Body>
-				{#each closestMatchups as closestMatchup, ix}
-				    <Row>
-					<Cell class="rank">{ix + 1}</Cell>
-					<Cell class="cellName center differentialName">
-					    <div class="center" on:click={() => gotoManager(closestMatchup.home.rosterID)}>
-						{closestMatchup.home.manager.name} ({round(closestMatchup.home.fpts)})
-						{#if !allTime  && cleanName(closestMatchup.home.manager.name) != cleanName(currentManagers[closestMatchup.home.rosterID].name)}
-						    <div class="curRecordManager">({currentManagers[closestMatchup.home.rosterID].name})</div>
-						{/if}
-					    </div>
-					    vs
-					    <div class="center" on:click={() => gotoManager(closestMatchup.away.rosterID)}>
-						{closestMatchup.away.manager.name} ({round(closestMatchup.away.fpts)})
-						{#if !allTime  && cleanName(closestMatchup.away.manager.name) != cleanName(currentManagers[closestMatchup.away.rosterID].name)}
-						    <div class="curRecordManager">({currentManagers[closestMatchup.away.rosterID].name})</div>
-						{/if}
-					    </div>
-					</Cell>
-					<Cell>{allTime ? closestMatchup.year + " - " : "" } {closestMatchup.week}</Cell>
-					<Cell>{round(closestMatchup.differential)}</Cell>
-				    </Row>
-				{/each}
-			    </Body>
-			</DataTable>
-		    </div>
-		{/if}
-	    {/if}
-    </div>
-</div>
+    {#if blowouts && blowouts.length}
+        <DataTable class="recordTable">
+            <Head>
+                <Row>
+                    <Cell class="header" colspan=4>{prefix} Largest Blowouts</Cell>
+                </Row>
+                <Row>
+                    <Cell class="header rank"></Cell>
+                    <Cell class="header">Matchup</Cell>
+                    <Cell class="header">Week</Cell>
+                    <Cell class="header">Diff</Cell>
+                </Row>
+            </Head>
+            <Body>
+                {#each blowouts as blowout, ix}
+                    <Row>
+                        <Cell class="rank">{ix + 1}</Cell>
+                        <Cell class="cellName center differentialName">
+                            <div class="center" on:click={() => gotoManager(blowout.home.rosterID)}>
+                                {blowout.home.manager.name} ({round(blowout.home.fpts)})
+                                {#if !allTime  && cleanName(blowout.home.manager.name) != cleanName(currentManagers[blowout.home.rosterID].name)}
+                                    <div class="curRecordManager">({currentManagers[blowout.home.rosterID].name})</div>
+                                {/if}
+                            </div>
+                            vs
+                            <div class="center" on:click={() => gotoManager(blowout.away.rosterID)}>
+                                {blowout.away.manager.name} ({round(blowout.away.fpts)})
+                                {#if !allTime  && cleanName(blowout.away.manager.name) != cleanName(currentManagers[blowout.away.rosterID].name)}
+                                    <div class="curRecordManager">({currentManagers[blowout.away.rosterID].name})</div>
+                                {/if}
+                            </div>
+                        </Cell>
+                        <Cell>{allTime ? blowout.year + " - " : "" } {blowout.week}</Cell>
+                        <Cell>{round(blowout.differential)}</Cell>
+                    </Row>
+                {/each}
+            </Body>
+        </DataTable>
+    {/if}
 
-<div class="buttonHolder">
-    <Group variant="outlined">
-	<Button class="selectionButtons" on:click={() => changeVictorie('largblow')} variant="{curDiff == 'largblow' ? "raised" : "outlined"}">
-	    <Label>Largest Blowouts</Label>
-	</Button>
-	<Button class="selectionButtons" on:click={() => changeVictorie('narrwin')} variant="{curDiff == 'narrwin' ? "raised" : "outlined"}">
-	    <Label>Narrowest Wins</Label>
-	</Button>
-    </Group>
-</div>
-
-<!-- <div class="buttonHolder">
-    <Group variant="outlined">
-        {#each victories as victorie, ix}
-            <Button class="selectionButtons" on:click={() => curDiff = ix} variant="{curDiff == ix ? "raised" : "outlined"}">
-                <Label>{victorie}</Label>
-            </Button>
-        {/each}
-    </Group>
-</div> -->
-
-<!--     {#if closestMatchups && closestMatchups.length}
+    {#if closestMatchups && closestMatchups.length}
         <DataTable class="recordTable">
             <Head>
                 <Row>
@@ -652,7 +567,7 @@
             </Body>
         </DataTable>
     {/if}
-</div> -->
+</div>
 
 <h4>{prefix} Rankings</h4>
 
