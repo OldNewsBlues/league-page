179bb98dfe79b03eb9ef4c49a1faa357a489e19f
OldNewsBlues <53295790+OldNewsBlues@users.noreply.github.com>
2021-10-02 20:16:18 -0500

    Update AllTimeRecords.svelte

--- src/lib/Records/AllTimeRecords.svelte
+++ src/lib/Records/AllTimeRecords.svelte
@@ -2,7 +2,7 @@
     import {round} from '$lib/utils/helper'
   	import RecordsAndRankings from './RecordsAndRankings.svelte';
 
-    export let leagueRosterRecords, leagueWeekRecords, leagueWeekLows, allTimeBiggestBlowouts, allTimeClosestMatchups, allTimeIndivSeasons, currentManagers, mostSeasonLongPoints, leastSeasonLongPoints, transactionTotals;
+    export let leagueRosterRecords, leagueWeekRecords, leagueWeekLows, allTimeBiggestBlowouts, allTimeClosestMatchups, currentManagers, mostSeasonLongPoints, leastSeasonLongPoints, transactionTotals;
 
     let winPercentages = [];
     let lineupIQs = [];
@@ -79,7 +79,6 @@
     {showTies}
     {winPercentages}
     {fptsHistories}
-    {allTimeIndivSeasons}
     {lineupIQs}
     {tradesData}
     {waiversData}
